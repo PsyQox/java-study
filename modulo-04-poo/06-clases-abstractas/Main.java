3890c86adf314a3ef941c0b7ba6ea435d612b815
PsyQox <luisrodrizza@gmail.com>
2026-02-17 10:18:32 -0600

    vas

---
+++ modulo-04-poo/06-clases-abstractas/Main.java
@@ -0,0 +1,6 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //Abstraccion: Ocultar los detalles de implementacion y mostrar solo lo esencial. define que debe hacer algo pero no como lo hace ( El como lo decide cada subclase )
+    }
+}
